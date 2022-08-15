ad403cf6c8221dd2ef0003705f1d717092407630
Aryo Dimas <aryodimas07@gmail.com>
2022-08-15 09:34:56 +0700

    views fix for index

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'landingpage/templates/index.html', {'hello': 'Hello World!'})
+    return render(request, 'index.html', {'hello': 'Hello World!'})
