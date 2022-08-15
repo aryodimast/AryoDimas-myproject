696097a55b7e9b6d053b73d56b40fc2cffd06bc2
Aryo Dimas <aryodimas07@gmail.com>
2022-08-15 14:35:37 +0700

    commit

--- as_exercise_day4/views.py
+++ as_exercise_day4/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.template import loader
+from django.template import context, loader
 from django.http import HttpResponse
 import requests
 
@@ -7,4 +7,12 @@ import requests
 def categories(request):
     res = requests.get("https://jsonplaceholder.typicode.com/users")
     # return HttpResponse(res.json())
-    return render(request, 'as_exercise_day4/templates/categories.html')
+    # return render(request, 'as_exercise_day4/templates/index.html')
+    # return render(request, 'landingpage/templates/index.html', {'users': res})
+
+    context = {
+        'res': res
+    }
+
+    template = loader.get_template('categories.html')
+    return HttpResponse(template.render(context))
