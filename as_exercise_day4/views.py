681220537d1b0fc6f433698de92e53215d8af891
Aryo Dimas <aryodimas07@gmail.com>
2022-08-15 14:12:04 +0700

    commit

--- as_exercise_day4/views.py
+++ as_exercise_day4/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 import requests
 
 # Create your views here.
 def categories(request):
     res = requests.get("https://jsonplaceholder.typicode.com/users")
-    return HttpResponse(res.json())
-    return render(request, 'as_exercise_day4/templates/index.html', {'res': res})
-    # return render(request, 'landingpage/templates/index.html', {'users': res})
+    # return HttpResponse(res.json())
+
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
